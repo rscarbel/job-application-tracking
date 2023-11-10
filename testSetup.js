1774859f16e9eb83bbd94ee846d352e822aff689
Ryan Scarbel <ryanscarbel@gmail.com>
2023-11-09 20:52:04 -0500

    moved test reset to its own file

--- testSetup.js
+++ testSetup.js
@@ -1,25 +1,11 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+import resetDatabase from "./prisma/resetDatabase";
 
 process.env.NODE_ENV = "test";
 process.env.DATABASE_URL =
   "postgres://postgres:@localhost:5432/application_tracking_test";
 
 try {
-  await prisma.user.deleteMany();
-  await prisma.company.deleteMany();
-  await prisma.applicationCard.deleteMany();
-  await prisma.job.deleteMany();
-  await prisma.jobAddress.deleteMany();
-  await prisma.userAddress.deleteMany();
-  await prisma.companyAddress.deleteMany();
-  await prisma.compensation.deleteMany();
-  await prisma.contact.deleteMany();
-  await prisma.contactAddress.deleteMany();
-  await prisma.contactAttribute.deleteMany();
-  await prisma.email.deleteMany();
-  await prisma.emailTemplate.deleteMany();
-  await prisma.oAuth.deleteMany();
+  await resetDatabase();
 } catch {
   console.error("\nThere was an error resetting your test database");
   console.log(
