80b2ff657dd2c0ad6d2adf643dd54561fd045317
Ryan Scarbel <ryanscarbel@gmail.com>
2023-11-11 09:35:45 -0500

    improved test seed messagining

--- testSetup.js
+++ testSetup.js
@@ -5,7 +5,7 @@ process.env.DATABASE_URL =
 try {
   require("./prisma/seed");
 } catch {
-  console.error("\nThere was an error resetting your test database");
+  console.error("\nThere was an error seeding your test database");
   console.log(
     "Make sure you have first created a postgres database called 'application_tracking_test' \nand then run 'bun run migrate:test' to prepare the test database\n"
   );
