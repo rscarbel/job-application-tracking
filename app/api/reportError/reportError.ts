1882f29d3256c3f5cacf66d82b040180e63d2f36
Ryan Scarbel <40727301+rscarbel@users.noreply.github.com>
2024-01-14 14:21:11 -0500

    converted report error to typescript (#13)

---
+++ app/api/reportError/reportError.ts
@@ -0,0 +1,61 @@
+import Bugsnag from "@bugsnag/js";
+
+if (process.env.NODE_ENV !== "development") {
+  Bugsnag.start({
+    apiKey: process.env.BUGSNAG_API_KEY || "",
+  });
+}
+
+interface User {
+  id?: string;
+  email?: string;
+  firstName?: string;
+  lastName?: string;
+}
+
+interface ErrorObject {
+  message: string;
+  stack?: string;
+  user?: User;
+}
+
+/**
+ * Helper function to report events.
+ *
+ * @param errorObject - The error object containing message, stack, and user details.
+ */
+export const reportError = (errorObject: ErrorObject): void => {
+  console.error(errorObject.message);
+  if (process.env.NODE_ENV === "development") return;
+
+  try {
+    const defaultErrorMessage = "An unknown error occurred";
+    const defaultErrorStack = "No stack trace available";
+
+    if (errorObject.user) {
+      const {
+        id = "unknown",
+        email = "unknown",
+        firstName,
+        lastName,
+      } = errorObject.user;
+      const userFullName =
+        [firstName, lastName].filter(Boolean).join(" ") || "Anonymous User";
+
+      Bugsnag.setUser(id, email, userFullName);
+    }
+
+    Bugsnag.notify(
+      new Error(errorObject.message || defaultErrorMessage),
+      (event) => {
+        event.addMetadata("info", {
+          message: errorObject.message || defaultErrorMessage,
+          stack: errorObject.stack || defaultErrorStack,
+          user: errorObject.user || "unknown",
+        });
+      }
+    );
+  } catch (error) {
+    console.error("Failed to report to Bugsnag:", error);
+  }
+};
