442fd517379eed828703dcc0eb8596f2338c2bd2
Ryan Scarbel <ryanscarbel@gmail.com>
2024-01-14 12:50:51 -0500

    converted file to typescript

--- app/api/applicationGroup/find/card/route.ts
+++ app/api/applicationGroup/find/card/route.ts
@@ -1,20 +1,20 @@
 import { getFormattedCardData } from "@/services/applicationService";
 import { getRequestUser } from "@/services/userService";
 import { getToken } from "next-auth/jwt";
+import serverErrorResponse from "@/app/api/serverErrorResponse";
 
 export async function GET(request) {
   const token = await getToken({ req: request });
   const { sub, provider } = token || { sub: null, provider: null };
   const { searchParams } = new URL(request.url);
 
+  if (typeof sub !== "string" || typeof provider !== "string") {
+    return serverErrorResponse("Unauthorized. Invalid credentials.", 401);
+  }
+
   const applicationId = parseInt(searchParams.get("applicationId"));
 
-  if (!applicationId) {
-    return Response.json({
-      status: 400,
-      body: { error: "Invalid applicationId" },
-    });
-  }
+  if (!applicationId) return serverErrorResponse("Invalid application id", 400);
 
   const user = await getRequestUser({ sub, provider });
 
@@ -23,12 +23,7 @@ export async function GET(request) {
     userId: user?.id,
   });
 
-  if (!card) {
-    return Response.json({
-      status: 404,
-      body: { error: "Card not found" },
-    });
-  }
+  if (!card) return serverErrorResponse("Card not found", 404);
 
   return Response.json({
     body: JSON.stringify(card),
