5400bd04418fefbbf8c80595c4d46f6b28ce40b5
Ryan Scarbel <40727301+rscarbel@users.noreply.github.com>
2024-01-15 09:48:32 -0500

    converted layout to typescript (#35)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { getServerSession } from "next-auth";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { getServerSession, Session } from "next-auth";
 import SessionProvider from "./components/SessionProvider";
 import NavMenu from "./components/NavMenu";
+import { ReactNode } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,8 +12,14 @@ export const metadata = {
   description: "Track your job applications",
 };
 
-export default async function RootLayout({ children }) {
-  const session = await getServerSession();
+interface RootLayoutProps {
+  children: ReactNode;
+}
+
+export default async function RootLayout({
+  children,
+}: RootLayoutProps): Promise<JSX.Element> {
+  const session: Session | null = await getServerSession();
   return (
     <html lang="en">
       <body className={inter.className}>
