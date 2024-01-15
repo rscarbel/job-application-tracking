1159da44594df6456649735eb57bed6ac8af2196
Ryan Scarbel <40727301+rscarbel@users.noreply.github.com>
2024-01-14 22:02:32 -0500

    Jat 26 board page to typescript (#27)
    * converted board page to typescript

--- app/board/page.tsx
+++ app/board/page.tsx
@@ -8,6 +8,17 @@ import options from "../api/auth/[...nextauth]/options";
 import "primereact/resources/themes/viva-light/theme.css";
 import "primeicons/primeicons.css";
 import { calculateBoardStructure } from "@/app/api/applicationGroup/calculateBoardStructure";
+import { BoardStructureInterface } from "../api/applicationGroup/BoardStructureInterface";
+
+const DEFAULT_EMPTY_BOARD: BoardStructureInterface = {
+  applications: {},
+  columns: {},
+  columnOrder: [],
+};
+
+const boardIsEmpty: (board: BoardStructureInterface) => boolean = (board) => {
+  return Object.keys(board.applications).length === 0;
+};
 
 const DynamicTextEditor = dynamic(() => import("./Board"), {
   ssr: false,
@@ -16,7 +27,7 @@ const DynamicTextEditor = dynamic(() => import("./Board"), {
 
 const getCardsForUser = async (email) => {
   const user = await prisma.user.findUnique({ where: { email } });
-  if (!user) return [];
+  if (!user) return DEFAULT_EMPTY_BOARD;
 
   const board = await prisma.applicationGroup.findFirst({
     where: { userId: user.id },
@@ -24,24 +35,30 @@ const getCardsForUser = async (email) => {
       createdAt: "desc",
     },
   });
-  if (!board) return [];
+  if (!board) return DEFAULT_EMPTY_BOARD;
 
-  const cards = await getFormattedCardsForBoard(board.id);
+  const cards = await getFormattedCardsForBoard({ groupId: board.id });
   const boardData = calculateBoardStructure(cards);
 
   return boardData;
 };
 
 const Job = async () => {
+  // @ts-ignore - there is a capability problem with the types in next-auth. The options are valid but typescript can't validate them.
   const session = await getServerSession(options);
 
   if (!session) {
     redirect("/api/auth/signin");
     return;
   }
-  const userEmail = session.user.email;
 
+  const userEmail = session.user.email;
   const board = await getCardsForUser(userEmail);
+
+  if (boardIsEmpty(board)) {
+    redirect("/create-card");
+    return;
+  }
 
   return <DynamicTextEditor board={board} />;
 };
