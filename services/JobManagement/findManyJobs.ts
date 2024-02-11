87ad6be0abea3e86c22e23b385c4b25a60efcb98
Ryan Scarbel <ryanscarbel@gmail.com>
2024-02-11 15:10:17 -0500

    took interface out of file

--- services/JobManagement/findManyJobs.ts
+++ services/JobManagement/findManyJobs.ts
@@ -1,62 +1,11 @@
 import prisma from "@/services/globalPrismaClient";
-import { TransactionClient } from "@/utils/databaseTypes";
 import {
   WorkMode,
   PayFrequency,
   CompanySize,
   CompanyType,
 } from "@prisma/client";
-
-interface IncludesInterface {
-  compensation?: boolean;
-  address?: boolean;
-  benefits?: boolean;
-  company?: boolean;
-}
-
-interface LocationInterface {
-  cities?: string[];
-  states?: string[];
-  countries?: string[];
-}
-
-interface FilterInterface {
-  companies?: string[];
-  companySizes?: CompanySize[];
-  companyTypes?: CompanyType[];
-  workModes?: WorkMode[];
-  benefits?: string[];
-  payMinimum?: number;
-  payMaximum?: number;
-  payFrequencies?: PayFrequency[];
-  currencies?: string[];
-  locations?: LocationInterface;
-  createdAt?: Date;
-}
-
-interface SelectInterface {
-  id?: boolean;
-  title?: boolean;
-  workMode?: boolean;
-  responsibilities?: boolean;
-  description?: boolean;
-  createdAt?: boolean;
-  updatedAt?: boolean;
-}
-
-interface PaginationInterface {
-  page: number;
-  per: number;
-}
-
-interface ManyJobsInterface {
-  userId: string;
-  include: IncludesInterface;
-  filters?: FilterInterface;
-  pagination?: PaginationInterface;
-  select?: SelectInterface;
-  client?: TransactionClient | typeof prisma;
-}
+import { ManyJobsInterface } from "./ManyJobsInterface";
 
 interface WhereInterface {
   userId: string;
